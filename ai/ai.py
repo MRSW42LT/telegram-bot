8d7534485f17b3cc287051af1676eed0258241da
denisborbasianhaki <81716552+denisborbasianhaki@users.noreply.github.com>
2022-06-30 21:29:41 -0300

    started to introduce flask framework

---
+++ ai/ai.py
@@ -0,0 +1,3 @@
+def generate_smart_reply(text):
+    # here we can do all our work
+    return "this is a smart reply from the ai!"
---
+++ app.py
@@ -0,0 +1,4 @@
+from flask import Flask, request
+
+# start the flask app
+app = Flask(__name__)
