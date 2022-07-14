8ded053c5073fdf26e58b70abe9a510e92310645
Denis Kehrwald <denis.ianhaki@escola.pr.gov.br>
2022-07-13 22:40:49 -0300

    sdf

--- Functions/generalUtils/libgen.py
+++ Functions/generalUtils/libgen.py
@@ -1,17 +1,1 @@
-def libgen():
-    """
-    Search for a book in libgen.io
-    """
-    title = "titulo"
-
-    from libgen_api import LibgenSearch
-
-    s = LibgenSearch()
-    results = s.search_title(title)
-    item_to_download = results[0]
-    download_links = s.resolve_download_links(item_to_download)
-
-    with open('filename.txt', 'w', encoding='utf-8') as f:
-        print(results[0], download_links, file=f)
-
-libgen()
+from libgen_api import LibgenSearch
