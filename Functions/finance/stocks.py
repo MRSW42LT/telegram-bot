ebbce018c963ba702554a5517cbf4e84a2015c31
Denis Kehrwald <denis.ianhaki@escola.pr.gov.br>
2022-07-16 22:01:29 -0300

    visual changes

--- Functions/finance/stocks.py
+++ Functions/finance/stocks.py
@@ -7,13 +7,14 @@ def stock(company):
 
     obj = yf.Ticker(str(company))
     historyData = obj.history(period='1y', start='1910-01-01', end=str(date.today()))
+    todayPrice = obj.history(period='1d', start=str(date.today()), end=str(date.today()))
+    todayPrice = todayPrice['Close'][0]
+    todayPrice = round(todayPrice, 2)
     plt.style.use('ggplot')
     plt.plot(historyData['Close'])
+    plt.title(str(company)+ ' -  $' + str(todayPrice))
     plt.ylabel('Price ($)')
     plt.xlabel('Date')
     plt.savefig('historyData.png')
-
-    todayPrice = obj.history(period='1d', start=str(date.today()), end=str(date.today()))
-    todayPrice = todayPrice['Close'][0]
     
     return str(todayPrice)
