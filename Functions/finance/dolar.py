edb52174bdf271a90fb4182e09f69e129086d58b
Denis Kehrwald <denis.ianhaki@escola.pr.gov.br>
2022-07-16 22:18:17 -0300

    cotarDolar function rewrite

--- Functions/finance/dolar.py
+++ Functions/finance/dolar.py
@@ -1,13 +1,21 @@
-import requests
-from bs4 import BeautifulSoup
+from datetime import date
+import yfinance as yf
+from matplotlib import pyplot as plt
+from matplotlib import style
 
 def cotar_dolar():
 
-    page = requests.get('https://dolarhoje.com/')
-
-    soup = BeautifulSoup(page.text, 'html.parser')
-
-    dolar_price = soup.find(class_='cotMoeda nacional')
-    dolar_price = dolar_price.find('input').get('value')
-
-    return str('US$1,0 vale R$'+dolar_price+' hoje.')
+    obj = yf.Ticker(str('USDBRL=X'))
+    historyData = obj.history(period='1y', start='1910-01-01', end=str(date.today()))
+    todayPrice = obj.history(period='1d', start=str(date.today()), end=str(date.today()))
+    todayPrice = todayPrice['Close'][0]
+    todayPrice = round(todayPrice, 3)
+    plt.style.use('ggplot')
+    plt.plot(historyData['Close'])
+    plt.title('Dólar hoje: R$' + str(todayPrice))
+    plt.ylabel('Preço (R$)')
+    plt.xlabel('Data')
+    plt.savefig(f'dolarHistoryData.png')
+    plt.close()
+    
+    return 'O preço atual de $1 em R$ é de: R$' + str(todayPrice)
