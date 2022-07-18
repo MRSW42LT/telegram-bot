68e409e2d7a00f32b399898d7d9344c909942895
Denis Kehrwald <denis.ianhaki@escola.pr.gov.br>
2022-07-17 23:24:38 -0300

    better dolar style

--- Functions/finance/dolar.py
+++ Functions/finance/dolar.py
@@ -7,13 +7,15 @@ def cotar_dolar():
 
     obj = yf.Ticker(str('USDBRL=X'))
     historyData = obj.history(period='1y', start='2022-01-01', end=str(date.today()))
-    todayPrice = obj.history(period='1d', start=str(date.today()), end=str(date.today()))
-    todayPrice = todayPrice['Close'][0]
+    todayPrice = obj.history(period='7d', start='2022-01-01', end=str(date.today()))
+    todayPrice = todayPrice['Close'].iloc[-1]
     todayPrice = round(todayPrice, 3)
-    plt.style.use('ggplot')
+    plt.style.use('seaborn')
     plt.plot(historyData['Close'])
     plt.title('Dólar hoje: R$' + str(todayPrice))
     plt.ylabel('Preço (R$)')
     plt.xlabel('Data')
     plt.savefig(f'dolarHistoryData.png')
     plt.close()
+    
+    return f'Dólar hoje: R$' + str(todayPrice)
